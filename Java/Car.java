2c9434253ab65821d8f3154d39ff79e52c1180ac
Santiago Lopez <santiagolopezpardo@gmail.com>
2022-06-16 19:39:44 -0500

    Used encapsulation, added getter and setter methods and verified the information for passenger seats for the car superclass

--- Java/Car.java
+++ Java/Car.java
@@ -1,8 +1,8 @@
 class Car {
-    Integer id;
-    String license;
-    Account driver;
-    Integer passengersNumber;
+    private Integer id;
+    private String license;
+    private Account driver;
+    private Integer passengersNumber;
 
     public Car(String license, Account driver) {
         this.license = license;
@@ -10,6 +10,37 @@ class Car {
     }
 
     void printCarData() {
-        System.out.println("License is: " + license + " Driver: " + driver.name);
+        System.out.println("License is: " + license + " Driver: " + driver.name + 
+        " with " + passengersNumber + " passenger seats.");
     }
+
+    public Integer getPassenger(){
+        return passengersNumber;
+    }
+
+    public void setPassenger(Integer passengersNumber){
+        if (passengersNumber == 4){
+            this.passengersNumber = passengersNumber;
+        }
+        else {
+            System.out.println("You can only assign 4 passengers");
+        }
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public Account getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Account driver) {
+        this.driver = driver;
+    }
+    
 }
--- Java/Main.java
+++ Java/Main.java
@@ -1,12 +1,13 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        Car car1 = new Car("AMQ123", new Account("Santiago Lopez", 1234567890));
-        car1.passengersNumber = 4;
+        UberX car1 = new UberX("AMQ123", new Account("Santiago Lopez", 1234567890), 
+        "Chevrolet", "sonic");
+        car1.setPassenger(4);
         car1.printCarData();
 
-        Car car2 = new Car("QMG843", new Account("Fulanito Detal", 987654321));
-        car2.passengersNumber = 3;
+        UberPool car2 = new UberPool("QMG843", new Account("Fulanito Detal", 987654321),"BMW", "X5");
+        car2.setPassenger(4);
         car2.printCarData();
 
         Driver Santiago = new Driver("Santiago Lopez", 123456789, "santiago@gmail.com", "aStrongPass123");
